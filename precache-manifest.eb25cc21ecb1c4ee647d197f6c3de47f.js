a8274db11847b62e6c67527e3ee6153979554716
Shefass <shefass@gmail.com>
2019-11-05 14:31:12 +0200

    Updates

---
+++ precache-manifest.eb25cc21ecb1c4ee647d197f6c3de47f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6faf24835193116a72ded1f94830bb6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0f9944014f0425ae5644",
+    "url": "/portfolio/static/css/2.7eafa3a7.chunk.css"
+  },
+  {
+    "revision": "dca9e534c171f12b1b3a",
+    "url": "/portfolio/static/css/main.c27371a7.chunk.css"
+  },
+  {
+    "revision": "0f9944014f0425ae5644",
+    "url": "/portfolio/static/js/2.195a4b9b.chunk.js"
+  },
+  {
+    "revision": "dca9e534c171f12b1b3a",
+    "url": "/portfolio/static/js/main.6840fc47.chunk.js"
+  },
+  {
+    "revision": "e8912043fa7243157f3f",
+    "url": "/portfolio/static/js/runtime-main.9d86131c.js"
+  },
+  {
+    "revision": "8e9a090c4a0625f60154f65285a6e40b",
+    "url": "/portfolio/static/media/Limusine.8e9a090c.jpg"
+  },
+  {
+    "revision": "6b79a5b9ff717d4ce3b6d171f135d2dd",
+    "url": "/portfolio/static/media/TeddyFrost.6b79a5b9.wav"
+  },
+  {
+    "revision": "beb4ba7aebc28c93f2e5bf6de3a34107",
+    "url": "/portfolio/static/media/blip.beb4ba7a.wav"
+  },
+  {
+    "revision": "36bb8e3a3fef2b1103b1afc17931bd9c",
+    "url": "/portfolio/static/media/bongo.36bb8e3a.wav"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "b5038584a82258e2fe145c23a7866184",
+    "url": "/portfolio/static/media/codepen.b5038584.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "758dd017afdc3d5c6285475b0581190c",
+    "url": "/portfolio/static/media/lenin.758dd017.jpg"
+  },
+  {
+    "revision": "d8026bac891cf868ac15f80fb1cc288b",
+    "url": "/portfolio/static/media/metalscrape.d8026bac.wav"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "93ab2582be28acd68b2275617833f63e",
+    "url": "/portfolio/static/media/pianoCsh.93ab2582.wav"
+  },
+  {
+    "revision": "d61e54da0fc17f6ddd1dde062dd717f3",
+    "url": "/portfolio/static/media/pianoFminor.d61e54da.wav"
+  },
+  {
+    "revision": "97dc50c7d2288ee32e675c1ca34fd7c6",
+    "url": "/portfolio/static/media/pianoLid.97dc50c7.wav"
+  },
+  {
+    "revision": "dabc3535fcfde72bfbe44f9efe699d5b",
+    "url": "/portfolio/static/media/pianoNot.dabc3535.mp3"
+  },
+  {
+    "revision": "e32b8520f3ce1be9f420a46e486d61cf",
+    "url": "/portfolio/static/media/tombola.e32b8520.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b4ed7343d6df94ded31b5642567ce599.js"
+  "/portfolio/precache-manifest.eb25cc21ecb1c4ee647d197f6c3de47f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
